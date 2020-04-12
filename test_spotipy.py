479703029cf27bdd960e70ece9a24214a8e6968b
Christina Markakis <cmmarkakis@gmail.com>
2020-04-12 14:22:30 -0400

    added 3 options for function

--- test_spotipy.py
+++ test_spotipy.py
@@ -9,25 +9,109 @@ SPOTIPY_REDIRECT_URI=#######
 
 scope = playlist-read-private
 
-
-#This function should return all of your playlists
-
+#OPT 1IF WE WANT TO PRINT INFO AS A RETURN
 def get_all_playlists(username):
-    playlists = sp.current_user_playlists(username) #got from spotipy documentation: https://readthedocs.org/projects/spotipy/downloads/pdf/latest/
-    playlist_list=[]
-    for playlist in playlists["items"]:
-        playlist_list.append(playlist['name'])) #made in case we want to use it as a list later
+    '''
+    Parameters
+    ----------
+    username : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    Prints all of a users playlists
+
+    '''
+    users_playlists = sp.current_user_playlists(username) #got from spotipy documentation: https://readthedocs.org/projects/spotipy/downloads/pdf/latest/
+    for playlist in users_playlists['items']: #looking through every item in the data we get from api     
         print()
         print(playlist['name'])
 
-#This function shoudl return all the tracks of a given playlist
+def get_all_tracks(playlist_id):
+    '''
+    assumes we have the playlist_id
+    Parameters
+    ----------
+    playlist_id : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    Prints all the tracks of a given playlist
+
+    '''
+    playlist_tracks = sp.playlist_tracks(playlist_id)
+    for track in playlist_track['items']:
+        print()
+        print(track['album']['name'])
+--------
+#OPT 2 - IF WE WANT TO STORE DATA AS A RETURN
+def get_all_playlists(username):
+    '''
+
+    Parameters
+    ----------
+    username : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    playlist : TYPE
+        DESCRIPTION.
+    returns all the users playlists
+
+    '''
+    results = sp.current_user_playlists(username) #got from spotipy documentation: https://readthedocs.org/projects/spotipy/downloads/pdf/latest/
+    for item in results['item']: #looking through every item in the data we get from api
+        playlist = items['name']
+        return playlist
+
 def get_all_tracks(playlist):
-    return
+    results = sp.current_user_playlists(username) #got from spotipy documentation: https://readthedocs.org/projects/spotipy/downloads/pdf/latest/
+    if playlist in results['items']:
+        for item in results['item']: #looking through every item in the data we get from api
+            playlist_id = results['item']['id']
+            playlist_tracks = sp.playlist_tracks(playlist_id)
+            return results['item']['name'] + playlist_tracks
+    else:
+        print('Playlist not found')
 
+---------    
+#OPT 3 -IF WE WANT TO MAKE ONE FUNCTION
+username = '' #need to figure out how to get username
 
+def get_user_playlists(username):
+    '''
+    assumes we know the username
+    
+    Parameters
+    ----------
+    username : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    TYPE
+        DESCRIPTION.
+    Returns a dictionary that has the playlist as the key and all the tracks
+
+    '''
+    results = sp.current_user_playlists(username) #got from spotipy documentation: https://readthedocs.org/projects/spotipy/downloads/pdf/latest/ 
+    user_playlists = {}
+    playlist_id = ''
+    
+    for playlist in results['items']: #looking through every item in the data we get from api
+        playlist_id = results['items']['id']
+        userplaylists[results['name']] = get_all_tracks(playlist)
+    
+    def get_all_tracks(playlist):
+        playlist_tracks = sp.playlist_tracks(playlist_id)
+        return playlist_tracks results['name']
+    
+    return user_playlists
+
+----
 #bloiler plate sample code
-
-
 birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
